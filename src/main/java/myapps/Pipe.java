f484853146ac871519c8f1e6180200cd89d0c3f0
방재근 <jgb710@naver.com>
2018-07-17 15:42:11 +0900

    modify pipe.java

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -4,9 +4,10 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.KafkaStreams;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Pipe {
 
@@ -14,7 +15,7 @@ public class Pipe {
     {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9200");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.171:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
@@ -22,5 +23,28 @@ public class Pipe {
         builder.stream("streams-plaintext-input").to("streams-to-output");
         final Topology topology = builder.build();
         System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("Streams-shutdown-hook")
+        {
+            @Override
+            public void run() {
+                streams.close();
+                System.out.println("topology started") ;
+                latch.countDown();
+                System.out.println("topology terminated");
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch(Throwable e)
+        {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 }
